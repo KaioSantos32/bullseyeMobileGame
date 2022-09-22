a22ed3be29d91b6e4eeb584bc4eb33a7c454f20e
kaioSantos32 <kaiohenriquesanto@gmail.com>
2022-09-22 17:30:36 -0300

    First part of app

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        binding.targetTextView.text = "48"
+
+        setContentView(view)
+
+    }
+}
