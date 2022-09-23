a6be645c06e5a294ef11573ce2de5ea634fbfa98
kaioSantos32 <kaiohenriquesanto@gmail.com>
2022-09-23 17:02:15 -0300

    Complete project

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.myapplication
 
-import android.app.ActionBar
 import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
+import kotlin.math.abs
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     private var sliderValue = 0
+    private var targetValue = Random.nextInt(1, 100)
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +22,16 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+
+        binding.targetTextView.text = targetValue.toString()
+
         binding.hitMeButton.setOnClickListener {
             Log.i("Button Click Listener",
                  "You Clicked on ME!")
             showResult()
         }
+
+
 
         binding.tagetValueNumber.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser:  Boolean) {
@@ -40,18 +48,39 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun showResult(){
-        val dialogTitle = getString(R.string.result_dialog_title)
-        val dialogMessage = getString(R.string.result_dialog_message, sliderValue)
+    private fun score(sliderValueGuess: Int): Int{
+        val maxScore = 100
+        var difference = abs(targetValue - sliderValue)
+        println(difference)
+        return maxScore - difference
+    }
 
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle(dialogTitle)
-        builder.setMessage(dialogMessage)
-        builder.setPositiveButton(R.string.result_dialog_button_text) {dialog, _->
-            dialog.dismiss()
+    private fun showResult(){
+
+        if ( sliderValue == targetValue ) {
+            val dialogTitle = getString(R.string.result_dialog_title_right)
+            val dialogMessage = getString(R.string.result_dialog_message_right, score(sliderValue))
+
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(dialogTitle)
+            builder.setMessage(dialogMessage)
+            builder.setPositiveButton(R.string.result_dialog_button_text_right) { dialog, _ ->
+                dialog.dismiss()
+            }
+            builder.create().show()
+        }else{
+            val dialogTitle = getString(R.string.result_dialog_title_wrong)
+            val dialogMessage = getString(R.string.result_dialog_message_wrong, sliderValue, score(sliderValue))
+
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(dialogTitle)
+            builder.setMessage(dialogMessage)
+            builder.setPositiveButton(R.string.result_dialog_button_text_wrong) { dialog, _ ->
+                dialog.dismiss()
+            }
+            builder.create().show()
         }
 
-        builder.create().show()
     }
 }
 
